279fa3f6490ee0225ad2eaf5c5895f6f1e0d58d9
tarappo <thirata@pplace.jp>
2018-06-28 00:30:56 +0900

    uppdate spec file

--- spec/ipa_info_action_spec.rb
+++ spec/ipa_info_action_spec.rb
@@ -1,9 +1,9 @@
 describe Fastlane::Actions::IpaInfoAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The ipa_info plugin is working!")
-
-      Fastlane::Actions::IpaInfoAction.run(nil)
+    it "throws exception" do
+      expect do
+        Fastlane::FastFile.new.parse("lane :test do ipa_info end").runner.execute(:test)
+      end.to raise_error(FastlaneCore::Interface::FastlaneError, 'You have to set path an ipa file')
     end
   end
 end
