1e2d4b713cbc17df2475efad0d0564133f83650f
tarappo <thirata@pplace.jp>
2018-06-28 00:06:54 +0900

    clear unused method

--- lib/fastlane/plugin/ipa_info/helper/ipa_info_helper.rb
+++ lib/fastlane/plugin/ipa_info/helper/ipa_info_helper.rb
@@ -5,11 +5,8 @@ module Fastlane
 
   module Helper
     class IpaInfoHelper
-      # class methods that you define here become available in your action
-      # as `Helper::IpaInfoHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the ipa_info plugin helper!")
+      def self.info_plist
+
       end
     end
   end
