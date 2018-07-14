e89e8c176c78cac0db3e42c76af11a8b0e7e00e5
tarappo <thirata@pplace.jp>
2018-07-14 21:58:52 +0900

    add change os name method

--- lib/fastlane/plugin/ipa_info/helper/ipa_info_helper.rb
+++ lib/fastlane/plugin/ipa_info/helper/ipa_info_helper.rb
@@ -6,6 +6,7 @@ module Fastlane
       def self.macos_build_to_macos_version(build:)
         # reference https://support.apple.com/ja-jp/HT201260
         case build
+        # macOS High Sierra
         when "17F77" then
           "10.13.5"
         when "17E199", "17E201" then
@@ -18,9 +19,35 @@ module Fastlane
           "10.13.1"
         when "17A365", "17A405" then
           "10.13"
+        # macOS Sierra
+        when "16G29", "16G1036", "16G1114", "16G1212" then
+          "10.12.6"
+        when "16F73" then
+          "10.12.5"
+        when "16E195" then
+          "10.12.4"
+        when "16D32" then
+          "10.12.3"
+        when "16C67" then
+          "10.12.2"
+        when "16B2555", "16B2557" then
+          "10.12.1"
+        when "16A323" then
+          "10.12.1"
         end
-
       end
+
+      def self.macos_version_to_os_name(version:)
+        minor_version = version.split(".")[1].to_i
+        # reference https://support.apple.com/ja-jp/HT201260
+        case minor_version
+        when 13
+          "macOS High Sierra"
+        when 12
+          "macOS Sierra"
+        end
+      end
+
     end
   end
 end
