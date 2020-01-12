2f22d3a04acf66c6efb8b0d82709ab1a7eb78bf7
tarappo <thirata@pplace.jp>
2020-01-12 09:05:41 +0900

    refactoring helper

--- lib/fastlane/plugin/ipa_info/helper/ipa_analyze_helper.rb
+++ lib/fastlane/plugin/ipa_info/helper/ipa_analyze_helper.rb
@@ -9,13 +9,13 @@ module Fastlane
     class IpaAnalyzeHelper
       def self.analyze(ipa_path)
         ipa_zipfile = Zip::File.open(ipa_path)
-        app_folder_path = find_app_folder_in_ipa(ipa_path, ipa_zipfile)
+        app_folder_path = find_app_folder_path_in_ipa(ipa_path)
         ipa_zipfile.close
 
-        mobileprovision_path = "#{app_folder_path}/embedded.mobileprovision"
-        mobileprovision_entry = ipa_zipfile.find_entry(mobileprovision_path)
+        # path
+        mobileprovision_entry = ipa_zipfile.find_entry("#{app_folder_path}/embedded.mobileprovision")
+        UI.user_error!("mobileprovision not found in #{ipa_path}") unless mobileprovision_entry
         info_plist_entry = ipa_zipfile.find_entry("#{app_folder_path}/Info.plist")
-
         UI.user_error!("Info.plist not found in #{ipa_path}") unless info_plist_entry
 
         return {
@@ -24,16 +24,13 @@ module Fastlane
         }
       end
 
+      # Info plist
       def self.analyze_info_plist(info_plist_entry, ipa_zipfile)
-        result = {
-            path_in_ipa: info_plist_entry.to_s,
-            content: {}
-        }
+        result = {}
 
         tempfile = Tempfile.new(::File.basename(info_plist_entry.name))
         begin
           ipa_zipfile.extract(info_plist_entry, tempfile.path) { true }
-          # convert from binary Plist to XML Plist
           UI.user_error!("Failed to convert binary Plist to XML") unless system("plutil -convert xml1 '#{tempfile.path}'")
 
           plist = Plist.parse_xml(tempfile.path)
@@ -41,23 +38,19 @@ module Fastlane
           plist.each do |key, value|
             parse_value = value.class == Hash || value.class == Array ? value : value.to_s
 
-            result[:content][key] = parse_value
+            result[key] = parse_value
           end
         rescue StandardError => e
-          puts e.message
-          result = nil
+          UI.user_error!(e.message)
         ensure
           tempfile.close && tempfile.unlink
         end
-        puts(result)
         return result
       end
 
+      # mobileprovisioning
       def self.analyze_mobileprovisioning(mobileprovision_entry, ipa_zipfile)
-        result = {
-            path_in_ipa: mobileprovision_entry.to_s,
-            content: {}
-        }
+        result = {}
 
         tempfile = Tempfile.new(::File.basename(mobileprovision_entry.name))
         begin
@@ -69,11 +62,10 @@ module Fastlane
 
             parse_value = value.class == Hash || value.class == Array ? value : value.to_s
 
-            result[:content][key] = parse_value
+            result[key] = parse_value
           end
         rescue StandardError => e
-          puts e.message
-          result = nil
+          UI.user_error!(e.message)
         ensure
           tempfile.close && tempfile.unlink
         end
@@ -81,27 +73,9 @@ module Fastlane
       end
 
       private
-      #
-      # Find the .app folder which contains both the "embedded.mobileprovision"
-      # and "Info.plist" files.
-      def self.find_app_folder_in_ipa(ipa_path, ipa_zipfile)
-        app_folder_in_ipa = "Payload/#{File.basename(ipa_path, File.extname(ipa_path))}.app"
-        info_plist_entry = ipa_zipfile.find_entry("#{app_folder_in_ipa}/Info.plist")
-
-        return app_folder_in_ipa if !info_plist_entry.nil?
-
-        ipa_zipfile.dir.entries('Payload').each do |dir_entry|
-          next unless dir_entry =~ /.app$/
-
-          app_folder_in_ipa = "Payload/#{dir_entry}"
-          info_plist_entry = ipa_zipfile.find_entry("#{app_folder_in_ipa}/Info.plist")
-
-          break if !info_plist_entry.nil?
-        end
-
-        UI.user_error!('No app folder found in the ipa file') if !info_plist_entry.nil?
-
-        return app_folder_in_ipa
+      # return app folder path
+      def self.find_app_folder_path_in_ipa(ipa_path)
+        return "Payload/#{File.basename(ipa_path, File.extname(ipa_path))}.app"
       end
     end
   end
